bd12fdda891b80a89603b20c3a1e2a48e169ec92
hti00036 <jake_tsao@htitec.com>
2023-02-21 10:11:50 +0800

    1. Build customized button for page index view.

--- app/src/main/java/com/example/pageindexview/view/item/NumberChip.kt
+++ app/src/main/java/com/example/pageindexview/view/item/NumberChip.kt
@@ -1,12 +1,20 @@
 package com.example.pageindexview.view.item
 
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.pageindexview.R
 
 @Preview
@@ -17,26 +25,40 @@ fun NumberChip(
     onClick: (Int) -> Unit = {}
 ) {
 
-    val colors = if (isOn) {
-        ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.blue))
+    val color = if (isOn) {
+        colorResource(id = R.color.blue)
     } else {
-        ButtonDefaults.buttonColors(containerColor = Color.Transparent)
+        Color.Transparent
     }
 
-    Button(
-        colors = colors,
-        onClick = { onClick(num - 1) },
+    val borderShape = RoundedCornerShape(36.dp)
+
+    Box(
+        modifier = Modifier
+            .size(50.dp)
+            .padding(4.dp)
+            .clip(borderShape)
+            .background(color)
+            .clickable(
+                onClick = { onClick(num - 1) }
+            ),
     ) {
-        NumberText(text = num.toString())
+        NumberText(
+            modifier = Modifier
+                .align(Alignment.Center),
+            text = num.toString()
+        )
     }
 
 }
 
 @Composable
 private fun NumberText(
+    modifier: Modifier,
     text: String,
 ) {
     Text(
+        modifier = modifier,
         text = text,
         color = colorResource(id = R.color.white)
     )
